16a7c05cae1664d8274a493cee22cdcd2ac2f53c
lunanarae <lunanarae@gmail.com>
2024-03-03 13:22:47 +0800

    add moment on the Javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-
+function updateTime() {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = document.querySelector("#london-date");
+  londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  let londonTimeElement = document.querySelector("#london-time");
+  londonTimeElement.innerHTML = moment()
+    .tz("Europe/London")
+    .format("h:mm:ss[<small>]A[</small>]");
+  let melbourneElement = document.querySelector("#melbourne");
+  let melbourneDateElement = document.querySelector("#melbourne-date");
+  melbourneDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  let melbourneTimeElement = document.querySelector("#melbourne-time");
+  melbourneTimeElement.innerHTML = moment()
+    .tz("Australia/Melbourne")
+    .format("h:mm:ss[<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
